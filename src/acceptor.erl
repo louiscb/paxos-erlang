83a014a9c8c5d4a0f2a24b32b9677f4554dc2ec5
Philipp Panzer <philipp.panzer@fau.de>
2022-09-29 19:44:32 +0200

    add code for sending messages with delay

--- src/acceptor.erl
+++ src/acceptor.erl
@@ -1,6 +1,9 @@
 -module(acceptor).
 -export([start/2]).
 
+-define(response_delay, 2000).
+-define(response_delay_enabled, false).
+
 start(Name, PanelId) ->
   spawn(fun() -> init(Name, PanelId) end).
 
@@ -10,12 +13,24 @@ init(Name, PanelId) ->
   Value = na,
   acceptor(Name, Promised, Voted, Value, PanelId).
 
+send_with_delay(Pid, Message) ->
+  T = rand:uniform(?response_delay),
+  timer:send_after(T, Pid, Message).
+
+send_message_to_proposer(Pid, Message) ->
+  case ?response_delay_enabled of
+    true ->
+      send_with_delay(Pid, Message);
+    false ->
+      Pid ! Message
+  end.
+
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
     {prepare, Proposer, Round} ->
       case order:gr(Round, Promised) of
         true ->
-          Proposer ! {promise, Round, Voted, Value},
+	  send_message_to_proposer(Proposer, {promise, Round, Voted, Value}),
           io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
             [Name, Round, Voted, Value]),
           % Update gui
@@ -24,7 +39,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
               "Promised: " ++ io_lib:format("~p", [Round]), Colour},
           acceptor(Name, Round, Voted, Value, PanelId);
         false ->
-          Proposer ! {sorry, {prepare, Round}},
+	  send_message_to_proposer(Proposer, {sorry, {prepare, Round}}),
 	  % It is wrong to pass Round here as it is smaller than Promised
 	  % acceptor(Name, Round, Voted, Value, PanelId)
           acceptor(Name, Promised, Voted, Value, PanelId)
@@ -32,7 +47,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
     {accept, Proposer, Round, Proposal} ->
       case order:goe(Round, Promised) of
         true ->
-          Proposer ! {vote, Round},
+	  send_message_to_proposer(Proposer, {vote, Round}),
           case order:goe(Round, Voted) of
             true ->
               io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
@@ -45,7 +60,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
               acceptor(Name, Promised, Voted, Value, PanelId)
           end;
         false ->
-          Proposer ! {sorry, {accept, Round}},
+	  send_message_to_proposer(Proposer, {sorry, {accept, Round}}),
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     stop ->
